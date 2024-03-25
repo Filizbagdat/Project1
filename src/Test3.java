ec36efe9c98335b1642cef1ea436a9ae2e2a6ffb
Filizbagdat <149052999+Filizbagdat@users.noreply.github.com>
2024-03-25 23:40:09 +0100

    Update Test3.java

--- src/Test3.java
+++ src/Test3.java
@@ -1,5 +1,7 @@
 public class Test3 {
     public static void main(String[] args) {
         System.out.println("test3 is now");
+        System.out.println("test4 is now");
+        //update
     }
 }
